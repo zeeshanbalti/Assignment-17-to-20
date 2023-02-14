2d29a7253d6300e87cefbddd07d7c140ccc42f14
zeeshanbalti <zeeshanbalti2003@gmail.com>
2023-02-14 14:50:37 +0500

    13 to 15 Assignment

---
+++ JavaScriptAssignement/jsassi13_15/app13_15.js
@@ -0,0 +1,158 @@
+// Question_1
+// Declare an empty array using JS literal notation to store
+// student names in future.
+
+// var nam = [];
+
+// Question_2
+// Declare an empty array using JS object notation to store
+// student names in future.
+
+// var nam = [];
+// var nam2 = new  array[];
+
+
+// Question_3
+
+
+// var names = ["zeeshan","Haider"];
+
+// Question_4
+
+
+// var num  = [2,4,6,8];
+
+// Question_5
+
+// var boolen = [true];
+
+// Question_6
+
+// var mixed = [1,"one",one];
+
+
+// Question_7
+
+
+/*document.write("<h1>Qualifications</h1>");
+var qualification =["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil","PhD"];
+    document.write("1)" + " " + qualification[0] + "<br>");
+    document.write("2)" + " " + qualification[1] + "<br>");
+    document.write("3)" + " " + qualification[2] + "<br>");
+    document.write("4)" + " " + qualification[3] + "<br>");
+    document.write("5)" + " " + qualification[4] + "<br>");
+    document.write("6)" + " " + qualification[5] + "<br>");
+    document.write("7)" + " " + qualification[6] + "<br>");
+    document.write("8)" + " " + qualification[7] + "<br>");*/
+
+
+    // Question_8
+
+// var stdName = ["zeeshan","Haider","Junaid"]+[200,300,400];
+
+
+
+
+// Question_9
+
+/*var colors = ["White","Green","Yellow","Red"];
+
+var colorName = prompt("Add the color to the beginning of the array.");
+if(colorName == "Gray"){
+    var colorName = colors.unshift("Gray");  
+}else if(colorName == "Pink"){
+    var colorName = colors.unshift("Pink");
+}else if(colorName == "Blue"){
+    var colorName = colors.unshift("Blue");
+}else{
+    var colorName = colors.unshift("Enter a Correct Colour");
+};
+ document.write(colors);*/
+
+//       b
+
+
+
+/*var colors = ["White","Green","Yellow","Red"];
+var colorName = prompt("Add that color to the end of the array.");
+if(colorName == "Gray"){
+    var colorName = colors.push("Gray");  
+}else if(colorName == "Pink"){
+    var colorName = colors.push("Pink");
+}else if(colorName == "Blue"){
+    var colorName = colors.push("Blue");
+}else{
+    var colorName = colors.push("Enter a Correct Colour");
+};
+ document.write(colors);*/
+
+
+
+//       C
+
+
+
+/*var colors = ["White","Green","Yellow","Red"];
+var colorName = prompt("Add two color to the beginning of the array.");
+if(colorName == "GrayBlue"){
+    var colorName = colors.unshift("Gray","Blue");  
+}
+else if(colorName == "YellowPink"){
+    var colorName = colors.unshift("Yellow","Pink");
+}else{
+    var colorName = colors.unshift("Enter a Correct Colour");
+};
+document.write(colors);*/
+
+
+
+//      D
+
+
+// var colors = ["White","Green","Yellow","Red"];
+//     colors.shift();
+//     document.write(colors);
+
+
+//       E
+
+// var colors  = ["White","Green","Yellow","Red"];
+// colors.pop();
+// document.write(colors)
+  
+
+//     F    
+
+
+/*var colors = ["White","Green","Yellow","Red"];
+var colorName = prompt("Add two color to the beginning of the array.");
+if(colorName == "GrayBlue"){
+    var colorName = colors.splice(0,0,"Gray","Blue");  
+}
+else if(colorName == "YellowPink"){
+    var colorName = colors.splice(0,0,"Yellow","Pink");
+}else{
+    var colorName = colors.splice(0,0,"Enter a Correct Colour");
+};
+document.write(colors);*/
+
+
+      
+
+
+
+
+
+
+    // Question_11
+
+    // var city = ["karachi","Lahore","Islamabad","Baltistan","Gilgit"];
+    // // document.write(city[2] +  city[4]);
+    // // console.log(city[2] +  city[4]);
+    // city.splice(3,2,);
+    // // city[0] = "Ghencha";
+    // document.write(city);
+
+
+ 
+    
