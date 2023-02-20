915c298fbe6c22732caaa443bb15144bd30bdc67
zeeshanbalti <zeeshanbalti2003@gmail.com>
2023-02-20 15:11:11 +0500

    assignment 17to 20

---
+++ JavaScriptAssignement/jsassi17_20/app17_20.js
@@ -0,0 +1,177 @@
+
+
+
+
+// Question_3
+
+/*for(i = 1 ; i<=10 ; i++){
+    document.write(i + "<br>");
+};*/
+
+
+// Question_4
+
+ /*var num = prompt("Enter an input");
+
+ var length = prompt("Enter a Length");
+
+ for(var i = 1 ; i <= length ; i++){
+    var result = i * num;
+    document.write(`${num} * ${i} = ${result} `+ "<br>");
+ };*/
+
+
+
+
+
+
+ 
+//  Question_5
+
+
+/*var fruit = ["apple", "banana", "mango", "orange" , "strawberry"];
+
+for( var i = 0;  i< fruit.length; i++){
+    if(fruit[0]){
+    document.write("Element at index 0 is " + fruit[0] +"<br>")
+    
+    }
+    if(fruit[1]){
+        document.write("Element at index 1 is " + fruit[1] +"<br>")
+    }
+    if(fruit[2]){
+        document.write("Element at index 2 is " + fruit[2] +"<br>")
+    }
+    if(fruit[3]){
+        document.write("Element  at index 3 is " + fruit[3] +"<br>")
+    }if(fruit[4]){
+        document.write("Element at index 4 is " + fruit[4] +"<br>")
+    };  
+    break;
+};*/
+
+
+
+//       Qustion_6
+
+ 
+            //       a
+//             var count = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+//             document.write("Count" + "<br>" + count + "<br>");
+
+//         //      b
+
+// var count = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+//  var counting = count.reverse()
+//  document.write("Reverse"+ "<br>" + counting);
+ 
+    //     c
+
+    // for(var i = 0 ; i < 21 ; i++){
+    //     if(i%2 == 0){
+    //         document.write(i)
+    //     }
+
+    // }
+
+
+        //   d
+
+        for(var i = 0 ; i < 21 ; i++){
+            if(i % 2 !== 0){
+            
+                document.write(i)
+            }
+        }
+
+
+// Question_7
+
+/*var A = ["apple pic", "Cookies","Cake","Chips","Patties"];
+
+var asked =  prompt("Welcome to ABC Cake");
+
+if(asked == A[0]){
+    document.write("Apple pic is available at Index 0 in Our bakery!");
+}else if(asked == A[1]){
+    document.write("Cookies is available at Index 1 in Our bakery!");
+}else if(asked == A[2]){
+    document.write("Cake is available at Index 2 in Our bakery!");
+
+}else if(asked == A[3]){
+    document.write("Chips is available at Index 3 in Our bakery!");
+}else if(asked == A[4]){
+    document.write("Patties is available at Index 4 in Our bakery!");
+}else{
+    document.write("We are Sorry " + asked + "is not available in Our bakery.")
+};*/
+
+
+
+
+// Question_8
+
+  
+
+
+
+
+
+// Question_10
+
+
+// for(var i = 5 ; i <= 100 ; i = i+5 ){
+//     document.write(i );
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+// var arr = prompt("Enter a pihcd");
+
+// for(var arr = i ;  arr>i;  i--){
+//     if(arr == i){
+//     document.write(i)
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var userAsk = prompt("asked a num");
+    var A = userAsk.split("");
+    var  B = A.reverse ("") 
+    var C = B.join("")
+if(userAsk == C){
+    document.write("True")
+
+}else{
+    document.write("False")
+};*/
